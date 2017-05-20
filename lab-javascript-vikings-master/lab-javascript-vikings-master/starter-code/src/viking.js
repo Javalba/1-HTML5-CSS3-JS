58fe644ca0f851f23f2613b8460020035f3a70c3
Javier Armero <Javalba90@gmail.com>
2017-05-20 18:13:33 +0200

    Refactoring code of vikings exercise. createSaxonArmy addWarriors (Vikings or Saxons) fight

--- lab-javascript-vikings-master/lab-javascript-vikings-master/starter-code/src/viking.js
+++ lab-javascript-vikings-master/lab-javascript-vikings-master/starter-code/src/viking.js
@@ -3,7 +3,6 @@
  * @param health
  * @param strength
  */
-
 function Soldier(health, strength) {
   this.strength = strength;
   this.health = health;
@@ -12,7 +11,6 @@ function Soldier(health, strength) {
    * Return the strength property of the Soldier
    * @return strength
    */
-
   this.attack = function() {
     return this.strength;
   };
@@ -22,7 +20,6 @@ function Soldier(health, strength) {
    * @param  damage
    * @return -
    */
-
   this.receiveDamage = function(damage) {
     this.health -= damage;
     if (health < 0) this.health = 0; // health can't be negative.
@@ -37,7 +34,6 @@ function Soldier(health, strength) {
  * @param strength
  * @param name
  */
-
 function Viking(name, health, strength) {
   Soldier.call(this, health, strength);
   this.name = name;
@@ -61,7 +57,6 @@ function Viking(name, health, strength) {
  * @param  health
  * @param  strength
  */
-
 function Saxon(health, strength) {
   Soldier.call(this, health, strength);
 
@@ -88,8 +83,11 @@ function War() {
   };
 
   this.vikingAttack = function() {
-    var saxon = this.saxonArmy[randomIndex(this.saxonArmy)];
-    var viking = this.vikingArmy[randomIndex(this.vikingArmy)];
+    var saxonIdx = randomIndex(this.saxonArmy);
+    var vikingIdx = randomIndex(this.vikingArmy);
+
+    var saxon = this.saxonArmy[saxonIdx];
+    var viking = this.vikingArmy[vikingIdx];
 
     if (this.vikingArmy.length === 0 || this.saxonArmy.length === 0) {
       return console.log("\n-");
@@ -140,6 +138,7 @@ function War() {
 
     if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
       console.info("[============= Vikings and Saxons are still in the thick of battle. =============]");
+      console.log("Vikings Army: " + this.vikingArmy.length + "\n" + "Saxons Army:" + this.saxonArmy.length);
     }
   };
 
@@ -156,74 +155,71 @@ function War() {
  * @param  array
  * @return index
  */
-
 function randomIndex(array) {
   return Math.floor(Math.random() * (array.length));
 }
 
 
 var viking1 = new Viking("Thor", 200, 50);
-var viking2 = new Viking("Ragnar", 100, 25);
+var viking2 = new Viking("Ragnar", 125, 25);
 var viking3 = new Viking("Ubbe", 100, 10);
 var viking4 = new Viking("Ivar", 100, 10);
 var viking5 = new Viking("Sigurd", 100, 10);
-var viking6 = new Viking("Ragnar", 100, 10);
+var viking6 = new Viking("Bjorn", 110, 15);
+var vikings = [viking1, viking2, viking3, viking4, viking5, viking6];
 
-var saxon1 = new Saxon(100, 20);
-var saxon2 = new Saxon(50, 5);
-var saxon3 = new Saxon(50, 5);
-var saxon4 = new Saxon(50, 5);
-var saxon5 = new Saxon(50, 5);
-var saxon6 = new Saxon(50, 5);
-var saxon7 = new Saxon(50, 5);
-var saxon8 = new Saxon(50, 5);
-var saxon9 = new Saxon(50, 5);
-var saxon10 = new Saxon(50, 5);
-var saxon11 = new Saxon(50, 5);
-var saxon12 = new Saxon(50, 5);
-var saxon13 = new Saxon(50, 5);
-var saxon14 = new Saxon(50, 5);
-var saxon15 = new Saxon(50, 5);
-var saxon16 = new Saxon(50, 5);
-var saxon17 = new Saxon(50, 5);
-var saxon18 = new Saxon(50, 5);
-var saxon19 = new Saxon(50, 5);
-var saxon20 = new Saxon(50, 5);
+/**
+ * Create a Saxon army. Introduce a number of warrioirs and return an array of them.
+ * All warriors have 50 health and 5 strenght
+ * @param warriors
+ * @return saxons
+ */
+function createSaxonArmy(warriors) {
+  var saxons = [];
+  for (var i = 0; i < warriors; i++) {
+    saxons.push(new Saxon(50, 5));
+  }
+  return saxons;
+}
+
+/**
+ * Add warriors
+ * @param soldiers
+ */
+function addWarriors(soldiers) {
+  var typeSoldier = "",
+    i;
+  if (soldiers[1] instanceof Viking) typeSoldier = "Viking";
+  if (soldiers[1] instanceof Saxon) typeSoldier = "Saxon";
+  switch (typeSoldier) {
+    case "Viking":
+      for (i = 0; i < soldiers.length; i++) {
+        war.addViking(soldiers[i]);
+      }
+      break;
+    case "Saxon":
+      for (i = 0; i < soldiers.length; i++) {
+        war.addSaxon(soldiers[i]);
+      }
+      break;
+    default:
+  }
+}
+
+/**
+ * Fight until one team die
+ * @return -
+ */
+function fight() {
+  while (war.saxonArmy.length > 0 && war.vikingArmy.length > 0) {
+    war.vikingAttack();
+    war.saxonAttack();
+    war.saxonAttack();
+    war.showStatus();
+  }
+}
 
 var war = new War();
-war.addViking(viking1);
-war.addViking(viking2);
-war.addViking(viking3);
-war.addViking(viking4);
-war.addViking(viking5);
-war.addViking(viking6);
-
-war.addSaxon(saxon1);
-war.addSaxon(saxon2);
-war.addSaxon(saxon3);
-war.addSaxon(saxon4);
-war.addSaxon(saxon5);
-war.addSaxon(saxon6);
-war.addSaxon(saxon7);
-war.addSaxon(saxon8);
-war.addSaxon(saxon9);
-war.addSaxon(saxon10);
-war.addSaxon(saxon11);
-war.addSaxon(saxon12);
-war.addSaxon(saxon13);
-war.addSaxon(saxon14);
-war.addSaxon(saxon15);
-war.addSaxon(saxon16);
-war.addSaxon(saxon17);
-war.addSaxon(saxon18);
-war.addSaxon(saxon19);
-war.addSaxon(saxon20);
-
-war.showInitialStatus();
-
-for (var i = 0; i < 65; i++) {
-  war.vikingAttack();
-  war.saxonAttack();
-  war.saxonAttack();
-  war.showStatus();
-}
+addWarriors(createSaxonArmy(28));
+addWarriors(vikings);
+fight();
