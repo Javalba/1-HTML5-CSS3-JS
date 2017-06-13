dfb2aefab53bade95d335f7fa3ad70021c2b8f22
Javier Armero <Javalba90@gmail.com>
2017-06-13 19:06:52 +0200

    Lodash completed

--- lab-javascript-lodash-exercises-master/lab-javascript-lodash-exercises-master/starter-code/lib/lodash.js
+++ lab-javascript-lodash-exercises-master/lab-javascript-lodash-exercises-master/starter-code/lib/lodash.js
@@ -1,7 +1,7 @@
 /* Iteration 1 - The Office */
 var opinions = ["This is the best job ever", "Satisfied", "At least I get paid", "I m looking for another job", "I don\' t want to answer"];
 var departaments = [];
-var departaments = fillDepartaments(5,10);
+var departaments = fillDepartaments(5, 10);
 console.log("OFFICE -->", departaments);
 
 /**
@@ -11,7 +11,9 @@ console.log("OFFICE -->", departaments);
  * @return  departaments
  */
 function fillDepartaments(qty, ops) {
-  departaments.push(_.times(qty, function(){return fillOpinions(ops);}));
+  departaments.push(_.times(qty, function() {
+    return fillOpinions(ops);
+  }));
   return departaments;
 }
 
@@ -21,8 +23,8 @@ function fillDepartaments(qty, ops) {
  * @param  ops quantity of opinions
  * @return departamnt
  */
-function fillOpinions (ops){
-  var departament=[];
+function fillOpinions(ops) {
+  var departament = [];
   for (var j = 0; j < ops; j++) {
     departament.push(_.sample(opinions));
   }
@@ -32,10 +34,29 @@ function fillOpinions (ops){
 
 /* Iteration 2 - Harry Potter */
 var pairsOfBirthdays = _.chunk(birthdays, 2);
+console.log("HOla");
+
+console.log("pairsOfBirthdays:\n"+pairsOfBirthdays);
+
 // Adding extra birthdays to the array
 var moreBirthdays = ["Lily Evans", "30 January", "James Potter", "27 March",
-                     "Dudley Dursley", "30 June", "Tom Riddle", "31 December"];
-var allBirthdays = function () { return _.concat(pairsOfBirthdays, _.chunk(moreBirthdays, 2));};
+  "Dudley Dursley", "30 June", "Tom Riddle", "31 December"
+];
+
+// var birthdays = ["Severus Snape", "9 January", "Arthur Weasley", "6 February",
+//                  "Ron Weasley", "1 March", "Remus Lupin", "10 March",
+//                  "Fred & George Weasley", "1 April", "Pomona Sprout", "15 May",
+//                   "Draco Malfoy", "5 June", "Dobby", "28 June",
+//                   "Neville Longbottom", "30 July", "Harry Potter", "31 July",
+//                   "Ginevra Weasley", "11 August", "Percy Weasley ", "22 August",
+//                   "Hermione Granger", "19 September", "Minerva McGonagall",
+//                   "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
+//                   "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
+//                   "6 December", "Charlie Weasley", "12 December",];
+
+var allBirthdays = function() {
+  return _.concat(pairsOfBirthdays, _.chunk(moreBirthdays, 2));
+};
 console.log(allBirthdays());
 
 
@@ -45,15 +66,15 @@ var badPsswd = "1123456";
 //Unique Characters
 function noRepeatChar(password) {
 
-  var result = _.filter(password, function (value, index, iteratee) {
+  var result = _.filter(password, function(value, index, iteratee) {
     return _.includes(iteratee, value, index + 1);
-    });
+  });
 
-    if (result.length < 1) {
-      console.log("good password");
-    } else {
-      console.log("AUCH, bad password");
-    }
+  if (result.length < 1) {
+    console.log("good password");
+  } else {
+    console.log("AUCH, bad password");
+  }
 }
 
 noRepeatChar(goodPsswd);
@@ -61,7 +82,7 @@ noRepeatChar(badPsswd);
 // Only digits
 var goodPsswd = "1234567890";
 var badPsswd = "12345678901234567890";
-var trimPassword = function (password) {
+var trimPassword = function(password) {
   return _.dropRight(password.split(""), 10).join("");
 };
 trimPassword(badPsswd);
@@ -69,22 +90,22 @@ trimPassword(badPsswd);
 
 // Abbey Road Studios
 // November is a good month
-var novemberArtists = function () {
+var novemberArtists = function() {
   return _.uniq(_.map(_.filter(abbeyRoadRecords, ["month", 11]), "artist"));
 };
 console.log(novemberArtists());
 // Artist who recorded the most times
-var bestArtist = function () {
+var bestArtist = function() {
   return _.first(_.orderBy(_.groupBy(abbeyRoadRecords, "artist"), ['Array', 'length'], ['asc', 'desc']))[0].artist;
 };
 console.log(bestArtist());
 // The Beatles and Abbey Road
-var lastBeatlesSong = function () {
+var lastBeatlesSong = function() {
   return _.orderBy(_.filter(abbeyRoadRecords, ["artist", "The Beatles"]), 'year', ['desc'])[0].year;
 };
 console.log(lastBeatlesSong());
 // Sixties Crazyness
-var sixtiesSong = function () {
+var sixtiesSong = function() {
   return _.orderBy(_.filter(abbeyRoadRecords, ["year", 1969]), 'month', ['desc'])[0];
 };
 console.log(sixtiesSong().artist + " recorded " + sixtiesSong().song);
